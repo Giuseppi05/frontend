cdb7cbf8ddaec8840e2f789987e34b85a53efedd
Giuseppi05 <giuseppi0501@gmail.com>
2024-12-05 20:30:50 -0500

    .

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
-    //"https://backendmdw.onrender.com/api",
+    baseURL: "https://backendmdw.onrender.com/api",
+    //"http://localhost:3000/api",
     withCredentials: true
 })
 
